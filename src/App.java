3112d0cd3b94177c1776a0fc0257c5b902af518b
Jalomies <142224339+Jalomies@users.noreply.github.com>
2023-09-07 12:23:57 +0300

    Tarkistaja
    - Lisätty tarkistaja joka käy läpi arvotun listan edelliset numerot ja ilmoittaa jos joku numeroista on sama

--- src/App.java
+++ src/App.java
@@ -6,16 +6,30 @@ public class App {
         Random arpoja = new Random();
         int [] arvotutLuvut = new int [7];
         int arvottuLuku = 0;
+        boolean samaLuku = false;
+        int tarkistus = 0;
+        int verrattava = 0;
 
 
         for (int i= 0; i < arvotutLuvut.length; i++) {
             arvottuLuku = arpoja.nextInt(1,36);
             arvotutLuvut[i] = arvottuLuku;
+
+             for (int j= 0; j < i; j++) {
+
+                tarkistus = arvotutLuvut[j];
+                verrattava = arvottuLuku;
+                if (tarkistus == verrattava) {
+                    samaLuku = true;;
+                    System.out.println("SAMA! "+tarkistus+" LUKU ARVO UUDESTAAN!");
+                }
+        
+            }
+            System.out.println(arvotutLuvut[i]);
+
         }
 
-         for (int i= 0; i < arvotutLuvut.length; i++) {
-            System.out.println(arvotutLuvut[i]);
-        }
+
     }
 
 
