3d64c0fa75f89c2132ee8945e05f969a1da9fe66
Jalomies <142224339+Jalomies@users.noreply.github.com>
2023-09-08 23:22:40 +0300

    Arvonnan tarkistus
    - Ohjelma tulostaa arvotun lottokierroksen
    - Ohjelma vertaa arvottua kierrosta koodiin syötettyyn lottoriviin ja kertoo kuinka monta numeroa meni oikein.

--- src/App.java
+++ src/App.java
@@ -5,22 +5,22 @@ public class App {
         
         Random arpoja = new Random();
         int [] arvotutLuvut = new int [7];
+        int [] lottoRivi = {1,2,3,4,5,6,7};
         int arvottuLuku = 0;
         boolean samaLuku = false;
         int tarkistus = 0;
         int verrattava = 0;
+        int arvattuluku = 0;
+        int oikeinMaara = 0;
 
-
-
-        for (int i= 0; i < arvotutLuvut.length; i++) {
-System.out.println("On uudelleenarvonta: "+samaLuku); 
+        for (int i= 0; i < arvotutLuvut.length; i++) { 
             arvottuLuku = arpoja.nextInt(1,36);
             arvotutLuvut[i] = arvottuLuku;
-System.out.println("ARVOTTU: "+arvottuLuku);
+System.out.println("\nARVOTTU: "+arvottuLuku);
   
 
              for (int j= 0; j < i; j++) {    
-                
+
                 verrattava = arvotutLuvut[j];
 System.out.println(verrattava+"V");
                 tarkistus = arvottuLuku;
@@ -42,11 +42,31 @@ System.out.println("SAMA! "+tarkistus+" LUKU ARVO UUDESTAAN!");
                 System.out.println("LISÄTTY LISTAAN: " + arvotutLuvut[i]);
             
         }
-        System.out.println("Arvotut numerot ovat: ");
+        System.out.println("\nArvottu kierros on: ");
         for (int i = 0; i < arvotutLuvut.length; i++) {
-            System.out.print(arvotutLuvut[i]+" ");
-
+            System.out.print(arvotutLuvut[i]+" ");  
         }
+        System.out.println("\n\nLottorivisi on: ");
+        for (int i = 0; i < arvotutLuvut.length; i++) {
+            System.out.print(lottoRivi[i]+" ");  
+        }
+
+        for (int i = 0; i < arvotutLuvut.length; i++) { 
+            arvottuLuku = arvotutLuvut[i];
+System.out.println("\nArvottu: "+arvottuLuku);            
+
+            for (int j = 0; j <lottoRivi.length; j++) {
+                arvattuluku = lottoRivi[j];
+System.out.print("Arvattu: "+arvattuluku);                
+                if (arvattuluku == arvottuLuku) {
+System.out.print(" Oikein!");                    
+                oikeinMaara ++;
+                 }
+                 System.out.println(" ");
+            }
+        }
+
+        System.out.println("\nSait "+oikeinMaara+" oikein.");
 
     }
 
